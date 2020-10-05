lassoan/VTK

commit 933371b3b3838caa0ac000da10adcfdf2056c4b1

A new, faster method for vtkPolyData::ComputeBounds()
Added new threaded methods in vtkPolyData and vtkBoundingBox
for computing GetBounds(). Added a comparison test for the
methods vtkPolyData::GetBounds(), vtkPoints::GetBounds(),
and vtkBoundingBox::ComputeBounds(). The speed improvements on
large meshes is approximately 10x.

Also, the new vtkCellArray API, and the vtkArrayDispatch
framework is used. Note, as before, this new vtkPolyData::GetBounds()
only considers points used by cells to determine the bounding box.

Performance considerations required tuning the vtkCellArray and
vtkCellArrayIterator to share memory when possible (i.e., avoid
copying). Also cleaned up documentation and API for these classes.
A test was added to ensure that iterator is functioning properly
(e.g., is faster, sharing storage buffer as appropriate).

Finally discovered an error in vtkParametricKuen which was
generating NaN on some systems, which messed up the GetBounds()
computation.

Common/DataModel/vtkBoundingBox.cxx
@@ -13,8 +13,15 @@
 
 =========================================================================*/
 #include "vtkBoundingBox.h"
+#include "vtkArrayDispatch.h"
+#include "vtkDataArrayRange.h"
 #include "vtkMath.h"
 #include "vtkPlane.h"
+#include "vtkPoints.h"
+#include "vtkSMPThreadLocal.h"
+#include "vtkSMPTools.h"
+
+#include <array>
 #include <cassert>
 #include <cmath>
 
@@ -618,4 +625,141 @@ bool vtkBoundingBox::IntersectPlane(double origin[3], double normal[3])
   }
 
   return true;
+}
+
+// ---------------------------------------------------------------------------
+// Support ComputeBounds()
+namespace
+{
+
+template <typename PointsT>
+struct FastBounds
+{
+  PointsT* Points;
+  const unsigned char* PointUses;
+  double* Bounds;
+  vtkSMPThreadLocal<std::array<double, 6> > LocalBounds;
+
+  FastBounds(PointsT* pts, const unsigned char* ptUses, double* bds)
+    : Points(pts)
+    , PointUses(ptUses)
+    , Bounds(bds)
+  {
+  }
+
+  void Initialize()
+  {
+    std::array<double, 6>& localBds = this->LocalBounds.Local();
+
+    localBds[0] = VTK_DOUBLE_MAX;
+    localBds[2] = VTK_DOUBLE_MAX;
+    localBds[4] = VTK_DOUBLE_MAX;
+
+    localBds[1] = VTK_DOUBLE_MIN;
+    localBds[3] = VTK_DOUBLE_MIN;
+    localBds[5] = VTK_DOUBLE_MIN;
+  }
+
+  void operator()(vtkIdType beginPtId, vtkIdType endPtId)
+  {
+    std::array<double, 6>& localBds = this->LocalBounds.Local();
+    const auto tuples = vtk::DataArrayTupleRange<3>(this->Points, beginPtId, endPtId);
+    const unsigned char usedConst[1] = { 1 };
+    const unsigned char* used =
+      (this->PointUses != nullptr ? this->PointUses + beginPtId : usedConst);
+
+    for (const auto tuple : tuples)
+    {
+      if (*used)
+      {
+        double x = static_cast<double>(tuple[0]);
+        double y = static_cast<double>(tuple[1]);
+        double z = static_cast<double>(tuple[2]);
+        localBds[0] = std::min(x, localBds[0]);
+        localBds[1] = std::max(x, localBds[1]);
+        localBds[2] = std::min(y, localBds[2]);
+        localBds[3] = std::max(y, localBds[3]);
+        localBds[4] = std::min(z, localBds[4]);
+        localBds[5] = std::max(z, localBds[5]);
+
+        if (this->PointUses != nullptr)
+        {
+          ++used;
+        }
+      }
+    }
+  }
+
+  void Reduce()
+  {
+    // Composite bounds from all threads
+    double xmin = VTK_DOUBLE_MAX;
+    double ymin = VTK_DOUBLE_MAX;
+    double zmin = VTK_DOUBLE_MAX;
+    double xmax = VTK_DOUBLE_MIN;
+    double ymax = VTK_DOUBLE_MIN;
+    double zmax = VTK_DOUBLE_MIN;
+
+    for (auto iter = this->LocalBounds.begin(); iter != this->LocalBounds.end(); ++iter)
+    {
+      xmin = std::min((*iter)[0], xmin);
+      xmax = std::max((*iter)[1], xmax);
+      ymin = std::min((*iter)[2], ymin);
+      ymax = std::max((*iter)[3], ymax);
+      zmin = std::min((*iter)[4], zmin);
+      zmax = std::max((*iter)[5], zmax);
+    }
+
+    this->Bounds[0] = xmin;
+    this->Bounds[1] = xmax;
+    this->Bounds[2] = ymin;
+    this->Bounds[3] = ymax;
+    this->Bounds[4] = zmin;
+    this->Bounds[5] = zmax;
+  }
+};
+
+// Hooks into dispatcher vtkArrayDispatch by providing a callable generic
+struct BoundsWorker
+{
+  template <typename PointsT>
+  void operator()(PointsT* pts, const unsigned char* ptUses, double* bds)
+  {
+    FastBounds<PointsT> fastBds(pts, ptUses, bds);
+    vtkSMPTools::For(0, pts->GetNumberOfTuples(), fastBds);
+  }
+};
+
+} // anonymous namespace
+
+// ---------------------------------------------------------------------------
+// Fast computing of bounding box from vtkPoints.
+void vtkBoundingBox::ComputeBounds(vtkPoints* pts, double bounds[6])
+{
+  return vtkBoundingBox::ComputeBounds(pts, nullptr, bounds);
+}
+
+// ---------------------------------------------------------------------------
+// Fast computing of bounding box from vtkPoints and optional array that marks
+// points that should be used in the computation.
+void vtkBoundingBox::ComputeBounds(vtkPoints* pts, const unsigned char* ptUses, double bounds[6])
+{
+  // Check for valid
+  vtkIdType numPts;
+  if (pts == nullptr || (numPts = pts->GetNumberOfPoints()) < 1)
+  {
+    bounds[0] = bounds[2] = bounds[4] = VTK_DOUBLE_MAX;
+    bounds[1] = bounds[3] = bounds[5] = VTK_DOUBLE_MIN;
+    return;
+  }
+
+  // Compute bounds: dispatch to real types, fallback for other types.
+  using vtkArrayDispatch::Reals;
+  using Dispatcher = vtkArrayDispatch::DispatchByValueType<Reals>;
+  BoundsWorker worker;
+
+  if (!Dispatcher::Execute(pts->GetData(), worker, ptUses, bounds))
+  { // Fallback to slowpath for other point types
+    worker(pts->GetData(), ptUses, bounds);
+  }
 }

Common/DataModel/Testing/Python/CMakeLists.txt
@@ -5,6 +5,8 @@ vtk_add_test_python(
   SelectionLoop.py
   TestBoxPlaneIntersection.py
   TestCopyStructuredData.py,NO_DATA,NO_VALID
+  TestGetBounds.py,NO_DATA,NO_VALID
+  TestGetBounds2.py,NO_DATA,NO_VALID
   TestNumericArrayImageData.py,NO_DATA,NO_VALID
   TestPartitionedData.py,NO_DATA,NO_VALID
   TestQuadricClustering.py

Common/DataModel/Testing/Python/TestGetBounds.py
@@ -1,0 +1,115 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import vtk
+import sys
+
+# Test speed of compute bounds in vtkPolyData, vtkPoints, and
+# vtkBoundingBox.
+
+# Control model size
+res = 500
+timer = vtk.vtkTimerLog()
+
+# Uncomment if you want to use as a little interactive program
+#if len(sys.argv) >= 2 :
+#    res = int(sys.argv[1])
+#else:
+#    res = 500
+
+# Data source. Note that different types of cells are created
+# to exercise the vtkPolyData::GetBounds() properly.
+plane = vtk.vtkPlaneSource()
+plane.SetResolution(res,res)
+
+edges = vtk.vtkFeatureEdges()
+edges.SetInputConnection(plane.GetOutputPort())
+#edges.ExtractAllEdgeTypesOff()
+edges.BoundaryEdgesOn()
+edges.ManifoldEdgesOff()
+edges.NonManifoldEdgesOff()
+edges.FeatureEdgesOff()
+
+t1 = vtk.vtkTransform()
+t1.Translate(-1.0,0,0)
+tf1 = vtk.vtkTransformPolyDataFilter()
+tf1.SetInputConnection(edges.GetOutputPort())
+tf1.SetTransform(t1)
+
+t2 = vtk.vtkTransform()
+t2.Translate(1.0,0,0)
+tf2 = vtk.vtkTransformPolyDataFilter()
+tf2.SetInputConnection(edges.GetOutputPort())
+tf2.SetTransform(t2)
+
+append = vtk.vtkAppendPolyData()
+append.AddInputConnection(tf1.GetOutputPort())
+append.AddInputConnection(plane.GetOutputPort())
+append.AddInputConnection(tf2.GetOutputPort())
+append.Update()
+
+output = append.GetOutput()
+points = output.GetPoints()
+box = [0.0,0.0,0.0,0.0,0.0,0.0]
+
+print("Input data:")
+print("\tNum Points: {0}".format(output.GetNumberOfPoints()))
+print("\tNum Cells: {0}".format(output.GetNumberOfCells()))
+
+# Currently vtkPolyData takes into account cells that are connected to
+# points; hence only connected points (i.e., points used by cells) are
+# considered.
+
+# Compute bounds on polydata
+points.Modified()
+timer.StartTimer()
+output.GetBounds(box)
+timer.StopTimer()
+time = timer.GetElapsedTime()
+print("vtkPolyData::ComputeBounds():")
+print("\tTime: {0}".format(time))
+print("\tBounds: {0}".format(box))
+
+assert box[0] == -1.5
+assert box[1] ==  1.5
+assert box[2] == -0.5
+assert box[3] ==  0.5
+assert box[4] ==  0.0
+assert box[5] ==  0.0
+
+# Uses vtkPoints::ComputeBounds() which uses threaded vtkSMPTools and
+# vtkArrayDispatch (see vtkDataArrayPrivate.txx). In other words, cell
+# connectivity is not taken into account.
+points.Modified()
+timer.StartTimer()
+points.GetBounds(box)
+timer.StopTimer()
+time = timer.GetElapsedTime()
+print("vtkPoints::ComputeBounds():")
+print("\tTime: {0}".format(time))
+print("\tBounds: {0}".format(box))
+
+assert box[0] == -1.5
+assert box[1] ==  1.5
+assert box[2] == -0.5
+assert box[3] ==  0.5
+assert box[4] ==  0.0
+assert box[5] ==  0.0
+
+# Uses vtkBoundingBox with vtkSMPTools. This method takes into account
+# an (optional) pointUses array to only consider selected points.
+bbox = vtk.vtkBoundingBox()
+timer.StartTimer()
+bbox.ComputeBounds(points,box)
+timer.StopTimer()
+time = timer.GetElapsedTime()
+print("vtkBoundingBox::ComputeBounds():")
+print("\tTime: {0}".format(time))
+print("\tBounds: {0}".format(box))
+
+assert box[0] == -1.5
+assert box[1] ==  1.5
+assert box[2] == -0.5
+assert box[3] ==  0.5
+assert box[4] ==  0.0
+assert box[5] ==  0.0

Common/DataModel/Testing/Python/TestGetBounds2.py
@@ -1,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import vtk
+import sys
+
+# Test bounds computation for mixed cells in polydata.
+# Create some points that are unused by any cell as well.
+polyData = vtk.vtkPolyData()
+pts = vtk.vtkPoints()
+verts = vtk.vtkCellArray()
+lines = vtk.vtkCellArray()
+polys = vtk.vtkCellArray()
+strips = vtk.vtkCellArray()
+
+pts.SetNumberOfPoints(13)
+pts.SetPoint(0, 0,0,0)
+pts.SetPoint(1, 1,0,0)
+pts.SetPoint(2, 2,0,0)
+pts.SetPoint(3, 3,0,0)
+pts.SetPoint(4, 4,0,0)
+pts.SetPoint(5, 3,1,0)
+pts.SetPoint(6, 4,1,0)
+pts.SetPoint(7, 5,0,0)
+pts.SetPoint(8, 6,0,0)
+pts.SetPoint(9, 5,1,0)
+pts.SetPoint(10, 6,1,0)
+pts.SetPoint(11, 7,0,0)
+pts.SetPoint(12, 8,0,0)
+
+verts.InsertNextCell(1)
+verts.InsertCellPoint(0)
+
+lines.InsertNextCell(2)
+lines.InsertCellPoint(1)
+lines.InsertCellPoint(2)
+
+polys.InsertNextCell(4)
+polys.InsertCellPoint(3)
+polys.InsertCellPoint(4)
+polys.InsertCellPoint(6)
+polys.InsertCellPoint(5)
+
+strips.InsertNextCell(4)
+strips.InsertCellPoint(7)
+strips.InsertCellPoint(8)
+strips.InsertCellPoint(9)
+strips.InsertCellPoint(10)
+
+polyData.SetPoints(pts)
+polyData.SetVerts(verts)
+polyData.SetLines(lines)
+polyData.SetPolys(polys)
+polyData.SetStrips(strips)
+
+box = [0.0,0.0,0.0,0.0,0.0,0.0]
+
+print("Input data:")
+print("\tNum Points: {0}".format(polyData.GetNumberOfPoints()))
+print("\tNum Cells: {0}".format(polyData.GetNumberOfCells()))
+
+# Currently vtkPolyData takes into account cells that are connected to
+# points; hence only connected points (i.e., points used by cells) are
+# considered.
+
+# Compute bounds on polydata
+polyData.GetBounds(box)
+
+assert box[0] == 0.0
+assert box[1] == 6.0
+assert box[2] == 0.0
+assert box[3] == 1.0
+assert box[4] == 0.0
+assert box[5] == 0.0

Common/DataModel/vtkBoundingBox.h
@@ -14,11 +14,11 @@ PURPOSE.  See the above copyright notice for more information.
 =========================================================================*/
 /**
  * @class   vtkBoundingBox
- * @brief   Fast Simple Class for dealing with 3D bounds
+ * @brief   Fast, simple class for dealing with 3D bounds
  *
  * vtkBoundingBox maintains a 3D axis aligned bounding box.  It is very light
  * weight and many of the member functions are in-lined so it is very fast.
- * It is not derived from vtkObject so it can be allocated on the stack
+ * It is not derived from vtkObject so it can be allocated on the stack.
  *
  * @sa
  * vtkBox
@@ -28,6 +28,8 @@ PURPOSE.  See the above copyright notice for more information.
 #define vtkBoundingBox_h
 #include "vtkCommonDataModelModule.h" // For export macro
 #include "vtkSystemIncludes.h"
+
+class vtkPoints;
 
 class VTKCOMMONDATAMODEL_EXPORT vtkBoundingBox
 {
@@ -67,6 +69,16 @@ public:
    */
   void SetBounds(const double bounds[6]);
   void SetBounds(double xMin, double xMax, double yMin, double yMax, double zMin, double zMax);
+  //@}
+
+  //@{
+  /**
+   * Compute the bounding box from an array of vtkPoints. It uses a fast
+   * path when possible. The second signature (with point uses) only considers
+   * points with ptUses[i] != 0 in the bounds calculation.
+   */
+  static void ComputeBounds(vtkPoints* pts, double bounds[6]);
+  static void ComputeBounds(vtkPoints* pts, const unsigned char* ptUses, double bounds[6]);
   //@}
 
   //@{

Common/DataModel/vtkCellTypes.h
@@ -154,6 +154,14 @@ public:
    */
   static int IsLinear(unsigned char type);
 
+  //@{
+  /**
+   * Methods for obtaining the arrays representing types and locations.
+   */
+  vtkUnsignedCharArray* GetCellTypesArray() { return this->TypeArray; }
+  vtkIdTypeArray* GetCellLocationsArray() { return this->LocationArray; }
+  //@}
+
 protected:
   vtkCellTypes();
   ~vtkCellTypes() override;

Common/DataModel/vtkPolyData.cxx
@@ -14,7 +14,9 @@
 =========================================================================*/
 #include "vtkPolyData.h"
 
+#include "vtkBoundingBox.h"
 #include "vtkCellArray.h"
+#include "vtkCellArrayIterator.h"
 #include "vtkCellData.h"
 #include "vtkEmptyCell.h"
 #include "vtkGenericCell.h"
@@ -29,12 +31,12 @@
 #include "vtkPolyVertex.h"
 #include "vtkPolygon.h"
 #include "vtkQuad.h"
+#include "vtkSMPTools.h"
+#include "vtkSmartPointer.h"
 #include "vtkTriangle.h"
 #include "vtkTriangleStrip.h"
 #include "vtkUnsignedCharArray.h"
 #include "vtkVertex.h"
-
-#include "vtkSmartPointer.h"
 
 #include <stdexcept>
 
@@ -463,58 +465,89 @@ void vtkPolyData::GetCellBounds(vtkIdType cellId, double bounds[6])
 }
 
 //----------------------------------------------------------------------------
+// This method only considers points that are used by one or more cells. Thus
+// unused points make no contribution to the bounding box computation. This
+// is more costly to compute than using just the points, but for rendering
+// and historical reasons, produces preferred results.
 void vtkPolyData::ComputeBounds()
 {
   if (this->GetMeshMTime() > this->ComputeTime)
   {
-    // If there are no cells, but there are points, back to the
-    // bounds of the points set.
-    if (this->GetNumberOfCells() == 0 && this->GetNumberOfPoints())
+    // If there are no cells, but there are points, compute the bounds from the
+    // parent class vtkPointSet (which just examines points).
+    vtkIdType numPts = this->GetNumberOfPoints();
+    vtkIdType numCells = this->GetNumberOfCells();
+    if (numCells <= 0 && numPts > 0)
     {
       vtkPointSet::ComputeBounds();
       return;
     }
 
-    int t, i;
-    const vtkIdType* pts = nullptr;
-    vtkIdType npts = 0;
-    double x[3];
+    // We are going to compute the bounds
+    this->ComputeTime.Modified();
 
-    vtkCellArray* cella[4];
-
-    cella[0] = this->GetVerts();
-    cella[1] = this->GetLines();
-    cella[2] = this->GetPolys();
-    cella[3] = this->GetStrips();
-
-    // carefully compute the bounds
-    int doneOne = 0;
-    this->Bounds[0] = this->Bounds[2] = this->Bounds[4] = VTK_DOUBLE_MAX;
-    this->Bounds[1] = this->Bounds[3] = this->Bounds[5] = -VTK_DOUBLE_MAX;
-
-    // Iterate over cells's points
-    for (t = 0; t < 4; t++)
-    {
-      for (cella[t]->InitTraversal(); cella[t]->GetNextCell(npts, pts);)
-      {
-        for (i = 0; i < npts; i++)
-        {
-          this->Points->GetPoint(pts[i], x);
-          this->Bounds[0] = (x[0] < this->Bounds[0] ? x[0] : this->Bounds[0]);
-          this->Bounds[1] = (x[0] > this->Bounds[1] ? x[0] : this->Bounds[1]);
-          this->Bounds[2] = (x[1] < this->Bounds[2] ? x[1] : this->Bounds[2]);
-          this->Bounds[3] = (x[1] > this->Bounds[3] ? x[1] : this->Bounds[3]);
-          this->Bounds[4] = (x[2] < this->Bounds[4] ? x[2] : this->Bounds[4]);
-          this->Bounds[5] = (x[2] > this->Bounds[5] ? x[2] : this->Bounds[5]);
-          doneOne = 1;
-        }
-      }
-    }
-    if (!doneOne)
+    // Make sure this vtkPolyData has points.
+    if (this->Points == nullptr || numPts <= 0)
     {
       vtkMath::UninitializeBounds(this->Bounds);
+      return;
     }
-    this->ComputeTime.Modified();
+
+    // With cells available, loop over the cells of the polydata.
+    // Mark points that are used by one or more cells. Unmarked
+    // points do not contribute.
+    unsigned char* ptUses = new unsigned char[numPts];
+    std::fill_n(ptUses, numPts, 0); // initially unvisited
+
+    vtkCellArray* cellA[4];
+    cellA[0] = this->GetVerts();
+    cellA[1] = this->GetLines();
+    cellA[2] = this->GetPolys();
+    cellA[3] = this->GetStrips();
+
+    // Process each cell array separately. Note that threading is only used
+    // if the model is big enough (since there is a cost to spinning up the
+    // thread pool).
+    for (auto ca = 0; ca < 4; ca++)
+    {
+      if ((numCells = cellA[ca]->GetNumberOfCells()) > 0)
+      //      if ( (numCells=cellA[ca]->GetNumberOfCells()) > 250000 )
+      {
+        // Lambda to parallel compute bounds
+        vtkSMPTools::For(0, numCells, [&](vtkIdType cellId, vtkIdType endCellId) {
+          for (; cellId < endCellId; ++cellId)
+          {
+            // Don't move these variable outside the lamda, results in a huge performance hit
+            vtkIdType npts, ptIdx;
+            const vtkIdType* pts;
+            cellA[ca]->GetCellAtId(cellId, npts, pts);
+            for (ptIdx = 0; ptIdx < npts; ++ptIdx)
+            {
+              ptUses[pts[ptIdx]] = 1;
+            }
+          }
+        }); // end lambda
+      }
+      else
+      //      else if ( numCells > 0 ) // serial
+      {
+        auto iter = vtk::TakeSmartPointer(cellA[ca]->NewIterator());
+        for (iter->GoToFirstCell(); !iter->IsDoneWithTraversal(); iter->GoToNextCell())
+        {
+          vtkIdType npts, ptIdx;
+          const vtkIdType* pts;
+          iter->GetCurrentCell(npts, pts);
+          for (ptIdx = 0; ptIdx < npts; ++ptIdx)
+          {
+            ptUses[pts[ptIdx]] = 1;
+          }
+        }
+      }
+    } // for all cell arrays
+
+    // Perform the bounding box computation
+    vtkBoundingBox::ComputeBounds(this->Points, ptUses, this->Bounds);
+    delete[] ptUses;
   }
 }
 

Common/DataModel/vtkPolyData.h
@@ -133,7 +133,9 @@ public:
   void GetPointCells(vtkIdType ptId, vtkIdList* cellIds) override;
 
   /**
-   * Compute the (X, Y, Z)  bounds of the data.
+   * Compute the (X, Y, Z)  bounds of the data. Note that the method only considers
+   * points that are used by cells (unless there are no cells, in which case all
+   * points are considered). This is done for usability and historical reasons.
    */
   void ComputeBounds() override;
 

commit 92b1ee09385c271a88dc4d38d8ce4e4dc5940d9f

BUG: Fix bounding box computation
This commit fixes regression in bounding box computation in vtkBoundingBox class
introduced by https://github.com/Kitware/VTK/commit/933371b3b3838caa0ac000da10adcfdf2056c4b1.
See bug report from user here: https://discourse.vtk.org/t/getbounds-fails-with-nan-points-in-vtk9/4351.

A variable that may contain NaN must not be used as a first argument in comparison,
ternary, or min/max operations, because the result will be NaN. Instead, such variables must
be used as the second second argument. Fix is similar to this commit:
https://github.com/Kitware/VTK/commit/06d553fb232ac3992b99df47613506f106d2f46d

Common/DataModel/vtkBoundingBox.cxx
@@ -730,12 +730,12 @@ struct FastBounds
         double y = static_cast<double>(tuple[1]);
         double z = static_cast<double>(tuple[2]);
 
-        localBds[0] = std::min(x, localBds[0]);
-        localBds[1] = std::max(x, localBds[1]);
-        localBds[2] = std::min(y, localBds[2]);
-        localBds[3] = std::max(y, localBds[3]);
-        localBds[4] = std::min(z, localBds[4]);
-        localBds[5] = std::max(z, localBds[5]);
+        localBds[0] = std::min(localBds[0], x);
+        localBds[1] = std::max(localBds[1], x);
+        localBds[2] = std::min(localBds[2], y);
+        localBds[3] = std::max(localBds[3], y);
+        localBds[4] = std::min(localBds[4], z);
+        localBds[5] = std::max(localBds[5], z);
       }
       if (this->PointUses != nullptr)
       {
@@ -756,12 +756,12 @@ struct FastBounds
 
     for (auto iter = this->LocalBounds.begin(); iter != this->LocalBounds.end(); ++iter)
     {
-      xmin = std::min((*iter)[0], xmin);
-      xmax = std::max((*iter)[1], xmax);
-      ymin = std::min((*iter)[2], ymin);
-      ymax = std::max((*iter)[3], ymax);
-      zmin = std::min((*iter)[4], zmin);
-      zmax = std::max((*iter)[5], zmax);
+      xmin = std::min(xmin, (*iter)[0]);
+      xmax = std::max(xmax, (*iter)[1]);
+      ymin = std::min(ymin, (*iter)[2]);
+      ymax = std::max(ymax, (*iter)[3]);
+      zmin = std::min(zmin, (*iter)[4]);
+      zmax = std::max(zmax, (*iter)[5]);
     }
 
     this->Bounds[0] = xmin;